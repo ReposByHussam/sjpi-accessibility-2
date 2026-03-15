cf756fbb891e0a000de93cea6f80d4c1bc5b4b57
Hussam Hilow <developedbyhussam@gmail.com>
2026-03-16 00:58:33 +0100

    tog bort tidigare javascript kod och ladetill en filter funktion för knapparna

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
-// Simulerad blockering av tråden
-setInterval(() => {
-    let start = Date.now()
-    while (Date.now() - start < 1000) {}
-  }, 3000)
-  
-  // Förhindrar användaren att navigera bakåt
-  history.pushState(null, "", location.href)
-  window.onpopstate = () => history.go(1)
-  
-  // Visar innehållet på sidan efter 5 sekunder
-  setTimeout(() => {
-    document.body.style.display = "block"
-  }, 5000)
-  
-  // Loggar 10000 tal till konsolen efter 1 sekund
-  setTimeout(() => {
-    for (let i = 0; i < 10000; i++) {
-      console.log(i)
-    }
-  }, 1000)
-  
-  // Loggar 100 000 siffror direkt när sidan laddas
-  for (let i = 0; i < 100000; i++) {
-    console.log(i)
-  }
+document.addEventListener('DOMContentLoaded', () => {
+  const filterbuttons = document.querySelectorAll('.filter-btn');
+
+  filterbuttons.forEach(button => {
+    button.addEventListener('click', () => {
+      filterbuttons.forEach((btn)=> { 
+        btn.classList.remove('active'),
+        btn.setAttribute('aria-pressed', 'false')
+      });
+      button.classList.add('active');
+      button.setAttribute('aria-pressed', 'true');
+    });
+  });
+});
